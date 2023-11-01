351505c35dda261c4971a52a06c64e21cad3dea8
younjs <younjs@uniquegood.biz>
2023-11-01 16:18:44 +0900

    fix-image processing

--- src/ImageConverter.cpp
+++ src/ImageConverter.cpp
@@ -11,24 +11,47 @@ namespace image_converter {
             gray = img;
         }
 
-        float imgRatio = (float) gray.cols / (float) gray.rows;
-        int cropW = gray.cols;
-        int cropH = (int) round((float) gray.rows * imgRatio);
-        int cropX = (int) round((float) (gray.cols - cropW) / 2.0f);
-        int cropY = (int) round((float) (gray.rows - cropH) / 2.0f);
-        cv::Rect roi(cropX, cropY, cropW, cropH);
-        gray = gray(roi);
-        if (gray.cols != Constants::sourceImageW || gray.rows != Constants::sourceImageH) {
-            cv::resize(gray, gray, {Constants::sourceImageW, Constants::sourceImageH});
+        int tarWidth = Constants::sourceImageW;
+        int tarHeight = Constants::sourceImageH;
+
+        // Calculate the aspect ratio of the original image
+        float imgRatio = (float)gray.cols / (float)gray.rows;
+
+        // Calculate the new dimensions while maintaining the aspect ratio
+        int newWidth, newHeight;
+        if (imgRatio > 1.0) {
+            newWidth = tarWidth;
+            newHeight = (int)round(tarWidth / imgRatio);
+        } else {
+            newHeight = tarHeight;
+            newWidth = (int)round(tarHeight * imgRatio);
         }
-        for (int r = 0; r < gray.rows; r++) {
-            for (int c = 0; c < gray.cols; c++) {
-                if (gray.at<uchar>(r, c) == 0) {
-                    gray.at<uchar>(r, c) = 1;
+
+        // Create a new image with the target size and fill it with black
+        cv::Mat resizedImage(tarHeight, tarWidth, gray.type(), cv::Scalar(0));
+
+        // Calculate the ROI to paste the resized image
+        int x = (tarWidth - newWidth) / 2;
+        int y = (tarHeight - newHeight) / 2;
+        cv::Rect roi(x, y, newWidth, newHeight);
+
+        // Resize the original image to fit the new dimensions
+        cv::Mat resizedGray;
+        cv::resize(gray, resizedGray, cv::Size(newWidth, newHeight));
+
+        // Copy the resized image to the ROI
+        resizedGray.copyTo(resizedImage(roi));
+
+        // Ensure that any 0 values in the image are set to 1
+        for (int r = 0; r < resizedImage.rows; r++) {
+            for (int c = 0; c < resizedImage.cols; c++) {
+                if (resizedImage.at<uchar>(r, c) == 0) {
+                    resizedImage.at<uchar>(r, c) = 1;
                 }
             }
         }
+
         img.release();
-        return gray;
+        return resizedImage;
     }
 }
